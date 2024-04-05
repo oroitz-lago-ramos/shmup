c149087b89085ad17852bf837eefaa07c2e0f411
thanh-son-lemelle <thanh-son.lemelle@laplateforme.io>
2024-04-05 23:36:23 +0200

    <refactor>:<refactor player_ship_model class to include direction attribute>

--- model/Player_ship_model.py
+++ model/Player_ship_model.py
@@ -1,19 +1,22 @@
 from model.Moving_entity import Moving_entity
 class Player_ship_model(Moving_entity):
+    """A class that represents the player ship in the game. It inherits from the Moving_entity class.
+    arg: x, y, width, height, speed
+    attributes: x, y, width, height, speed, canons
+    """
     def __init__(self, x, y, width, height, speed):
-        super().__init__(x, y, width, height, speed)
-        self.direction = 1
+        super().__init__(x, y, width, height, speed, color=(255, 255, 255))
         self.canons = []
-        
-    def move(self, direction):
-        pass
-    
+
     def move_up(self):
         self.y -= self.speed
+        self.direction = 1
     def move_down(self):
         self.y += self.speed
+        self.direction = 2
     def move_left(self):
         self.x -= self.speed
+        self.direction = 3
     def move_right(self):
         self.x += self.speed
-        
+        self.direction = 4        
