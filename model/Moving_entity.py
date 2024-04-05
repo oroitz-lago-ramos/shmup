2292e03841682c411117da1c788c87a499debde7
thanh-son-lemelle <thanh-son.lemelle@laplateforme.io>
2024-04-05 23:35:43 +0200

    <refactor>:<refactor moving_entity class to include color attribute and move method>

--- model/Moving_entity.py
+++ model/Moving_entity.py
@@ -1,8 +1,12 @@
 from model.Entity_model import Entity_model
 
 class Moving_entity (Entity_model):
-    def __init__(self, x, y, width, height, image, speed):
-        super().__init__(x, y, width, height, image)
+    """A class that represents a moving entity in the game. It inherits from the Entity_model class.
+    arg: x, y, width, height, speed, image
+    attributes: x, y, width, height, speed, image, direction
+    """
+    def __init__(self, x, y, width, height, speed, color):
+        super().__init__(color, x , y, width, height)
         self.speed = speed
         self.direction = 1
 
@@ -22,9 +26,19 @@ class Moving_entity (Entity_model):
         self.x += self.speed
         self.direction = 4
 
+    def move(self):
+        if self.direction == 1:
+            self.move_up()
+        elif self.direction == 2:
+            self.move_down()
+        elif self.direction == 3:
+            self.move_left()
+        elif self.direction == 4:
+            self.move_right()
+
     def update(self):
         self.move()
-        self.rect.topleft = (self.x, self.y)
+
 
     def get_direction(self):
         return self.direction
