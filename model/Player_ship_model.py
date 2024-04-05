c23736c1e3d062ecebb61249d93fb4d784b7f212
thanh-son-lemelle <thanh-son.lemelle@laplateforme.io>
2024-04-05 21:59:16 +0200

    <refator><refactor Player_ship_model to inherit from moving_entity>

--- model/Player_ship_model.py
+++ model/Player_ship_model.py
@@ -1,20 +1,19 @@
-class Player_ship_model:
-    def __init__(self):
-        self.x = 500
-        self.y = 500
-        
-
-        
+from model.Moving_entity import Moving_entity
+class Player_ship_model(Moving_entity):
+    def __init__(self, x, y, width, height, speed):
+        super().__init__(x, y, width, height, speed)
+        self.direction = 1
+        self.canons = []
         
     def move(self, direction):
         pass
     
     def move_up(self):
-        self.y -= 20
+        self.y -= self.speed
     def move_down(self):
-        self.y += 20
+        self.y += self.speed
     def move_left(self):
-        self.x -= 20
+        self.x -= self.speed
     def move_right(self):
-        self.x += 20
+        self.x += self.speed
         
