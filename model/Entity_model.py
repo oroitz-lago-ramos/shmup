51a928be742c6fd79f8f139f151a22eb2fe175f1
oroitz-lago-ramos <oroitz.lago-ramos@laplateforme.io>
2024-04-05 16:44:49 +0200

    <create>[Entity_model.py]:<created the super class fro all entities in the game with setters and getters>

--- model/Entity_model.py
+++ model/Entity_model.py
@@ -1,3 +1,36 @@
 class Entity_model:
-    def __init__(self):
-        pass
+    def __init__(self, color, x, y, width, height):
+        self.color = color
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+    
+#==================================Getters and Setters==================================#
+        #==================Setters==================#
+    def set_color(self, color):
+        self.color = color
+    def set_x(self, x):
+        self.x = x
+    def set_y(self, y):
+        self.y = y
+    def set_width(self, width):
+        self.width = width
+    def set_height(self, height):
+        self.height = height
+
+        #==================Getters==================#
+    def get_color(self):
+        return self.color
+    def get_x(self):
+        return self.x
+    def get_y(self):
+        return self.y
+    def get_width(self):
+        return self.width
+    def get_height(self):
+        return self.height
+    def get_center(self):
+        return (self.x + self.width // 2, self.y + self.height // 2)
+
+        
