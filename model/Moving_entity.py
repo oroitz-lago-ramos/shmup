fb55922ff92aa4aa431c96bafca26c1b9205572e
thanh-son-lemelle <thanh-son.lemelle@laplateforme.io>
2024-04-06 12:59:31 +0200

    <refactor>:<refactor Moving_entity class for improved movement control>

--- model/Moving_entity.py
+++ model/Moving_entity.py
@@ -1,41 +1,60 @@
 from model.Entity_model import Entity_model
 
+from pygame import math
 class Moving_entity (Entity_model):
     """A class that represents a moving entity in the game. It inherits from the Entity_model class.
-    arg: x, y, width, height, speed, image
-    attributes: x, y, width, height, speed, image, direction
+    arg: x, y, width, height, acceleration, max_speed, color
+    attributes: x, y, width, height, direction, acceleration, max_speed, deceleration
     """
-    def __init__(self, x, y, width, height, speed, color):
+    def __init__(self, x, y, width, height,acceleration, max_speed, color):
         super().__init__(color, x , y, width, height)
-        self.velocity_x = 0
-        self.velocity_y = 0
-        self.speed = speed
+        self.direction = math.Vector2(0, 0)
+        self.velocity = math.Vector2(0, 0)
+        self.acceleration = acceleration
+        self.max_speed = max_speed
+        self.timer_decelerate = 0
+
+    def set_direction(self, direction):
+        self.direction = direction.normalize()
+        self.velocity = self.direction * self.velocity.length()
+
+    def get_direction(self):     
+        return self.direction
+    
+    #==================================BASIC VECTOR MOUVEMENT FOR ENTITY==================================#
 
     def move_up(self):
-        self.y -= self.speed
-        self.velocity_y = -self.speed
-        self.velocity_x = 0
+        direction = math.Vector2(0, -1)
+        self.set_direction(direction)
     
     def move_down(self):
-        self.y += self.speed
-        self.velocity_y = self.speed
-        self.velocity_x = 0
+        direction = math.Vector2(0, 1)
+        self.set_direction(direction)
     
     def move_left(self):
-        self.x -= self.speed
-        self.velocity_x = -self.speed
-        self.velocity_y = 0
+        direction = math.Vector2(-1, 0)
+        self.set_direction(direction)
     
     def move_right(self):
-        self.x += self.speed
-        self.velocity_x = self.speed
-        self.velocity_y = 0
+        direction = math.Vector2(1, 0)
+        self.set_direction(direction)
 
+    def accelerate(self):
+        print(" direction length ", self.direction.length())
+        if self.direction.length() > 0:
+            self.velocity += self.direction * self.acceleration
+            if self.velocity.length() > 0:
+                self.velocity.scale_to_length(min(self.max_speed, self.velocity.length()))
+
+    def stop(self): 
+        self.velocity = math.Vector2(0, 0)
+    
     def move(self):
-        self.x += self.velocity_x
-        self.y += self.velocity_y
-
+        self.x += self.velocity.x
+        self.y += self.velocity.y
+    
     def update(self):
+        self.accelerate()
         self.move()
 
 
