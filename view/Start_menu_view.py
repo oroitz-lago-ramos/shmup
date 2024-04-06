aac0f4a2453f4a6ee2a9b41f1061b44d92d80505
walid-saadelkhalk <walid.saad-el-khalid@laplateforme.io>
2024-04-06 14:58:50 +0200

    [CREATE] Start_menu_view render.py created

--- view/Start_menu_view.py
+++ view/Start_menu_view.py
@@ -11,7 +11,14 @@ class Start_menu_view(View_interface):
         self.text = Text()
     
     def draw(self):
-        self.text.draw_text(self.game.screen, "Press space to start", 50, 50, 30)
+        background_start_menu = pygame.image.load("assets/images/start_menu.png")
+        background_start_menu = pygame.transform.scale(background_start_menu, (self.game.screen.get_width(), self.game.screen.get_height()))
+        self.game.screen.blit(background_start_menu, (0, 0))
+        
+        start_font = pygame.font.Font('assets/font/pdark.ttf', 80)
+        self.text.draw_text(self.game.screen, "START", (self.game.screen.get_width()//2) - 100, self.game.screen.get_height()//1.37, 30, start_font, (255, 184, 28))
+
+
         
     def handle_event(self, event):
         if event.type == pygame.KEYDOWN:
