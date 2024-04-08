8e816d10772d600e235cdec42c780a897243dede
lyes-hamici <lyes.hamici@laplateforme.io>
2024-04-08 15:06:56 +0200

    <feat>[__init__.py]:<Add a Level_wave_model import in init.py>

--- model/__init__.py
+++ model/__init__.py
@@ -3,3 +3,4 @@ from .Entity_model import Entity_model
 from .Player_base_model import Player_base_model
 from .Projectile_model import Projectile_model
 from .Enemy_model import Enemy_model
+from .Level_wave_model import Level_wave_model
