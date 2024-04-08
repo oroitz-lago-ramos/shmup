15f42e1bc35a2fd4e074133d52708eab6c82015f
oroitz-lago-ramos <oroitz.lago-ramos@laplateforme.io>
2024-04-08 13:49:22 +0200

    Merge branch 'render_start' into debug_render_start

--- view/Start_menu_view.py
+++ view/Start_menu_view.py
@@ -12,10 +12,4 @@ class Start_menu_view(View_interface):
     
     def draw(self):
         self.text.draw_text(self.game.screen, "Press space to start", 50, 50, 30)
-        
-    def handle_event(self, event):
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                self.game.change_view(View_state.MAIN_MENU)
-            elif event.key == pygame.K_ESCAPE:
-                self.game.stop()
+    
