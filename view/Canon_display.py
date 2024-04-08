5828328bb9fa08a7aba37fa9aa92d2708d88a895
thanh-son-lemelle <thanh-son.lemelle@laplateforme.io>
2024-04-08 12:07:52 +0200

    <feat>:<add image rotation to canon_display class>

--- view/Canon_display.py
+++ view/Canon_display.py
@@ -6,6 +6,9 @@ class Canon_display (sprite.Sprite):
         super().__init__()  # Call the parent class (Sprite) constructor
         self.canon = canon_model
 
-
     def draw_on_center(self, screen, x , y):
-        pygame.draw.rect(screen, (0, 255, 0), (x + 10, y - 10, 10, 10))
+        self.image = pygame.draw.rect(screen, (0, 255, 0), (x + 10, y - 10, 10, 10))
+        self.update_image_rotation(self.canon.calcule_angle(pygame.mouse.get_pos()))
+    
+    def update_image_rotation(self, angle):
+        self.image = pygame.transform.rotate(self.image, angle)
