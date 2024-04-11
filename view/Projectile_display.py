328cd7e05c59240a2f71b6b3c32bb2728367452c
thanh-son-lemelle <thanh-son.lemelle@laplateforme.io>
2024-04-11 13:01:10 +0200

    <refactor>[projectile_display.py]<changeto rotate projectile image based on angle>

--- view/Projectile_display.py
+++ view/Projectile_display.py
@@ -8,7 +8,7 @@ class Projectile_display:
         self.image = self.load_image()
 
     def draw(self, x, y, angle, screen):
-            #pygame.draw.circle(screen, (255, 0, 0), (int(x), int(y)), radius)
+            # pygame.draw.circle(screen, (0, 255, 0), (int(x), int(y)), 10)
             self.rect = self.image.get_rect(center=(x,y))
             image = pygame.transform.rotate(self.image, (-(math.degrees(angle)+90)))
             screen.blit(image, self.rect)
