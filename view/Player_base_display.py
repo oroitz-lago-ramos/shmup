a62dddc8dedb1a5bc0351bcb0ae54091ad944f05
lyes-hamici <lyes.hamici@laplateforme.io>
2024-04-11 10:55:01 +0200

    <rafactor>[Player_base_dispaly.py]:<Replace the circle by the earth>

--- view/Player_base_display.py
+++ view/Player_base_display.py
@@ -7,9 +7,14 @@ class Player_base_display:
         self.rect = pygame.Rect(self.x, self.y, 77, 65)
         self.image = pygame.image.load("assets\\images\\background\\player_base.png") 
         self.image = pygame.transform.scale(self.image, (self.rect.width, self.rect.height))  
+        self.image_planet = pygame.image.load("assets\\images\\background\\earth.png") 
         
     def draw_on_center(self, screen, radius):
-        pygame.draw.circle(screen, (100, 100, 100), self.rect.center, radius)
+        diameter = 2 * radius
+        planet_image_resized = pygame.transform.scale(self.image_planet, (diameter, diameter))
+        planet_rect = planet_image_resized.get_rect()
+        planet_rect.center = self.rect.center
+        screen.blit(planet_image_resized, planet_rect)
         screen.blit(self.image, self.rect)
     
     def update(self,screen, x, y, radius):
