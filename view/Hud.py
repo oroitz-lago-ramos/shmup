750c28d9ec3b6c74b6d055c120d3181314a30984
thanh-son-lemelle <thanh-son.lemelle@laplateforme.io>
2024-04-12 11:36:46 +0200

    <feat>:<update timer logic in Hud.py to display wave and timer information>

--- view/Hud.py
+++ view/Hud.py
@@ -4,12 +4,14 @@ class Hud:
     def __init__(self,game) -> None:
         self.game = game
         self.text = Text()
+        self.current_wave = None
+        self.timer = None
         self.stats_bar_img = pygame.image.load("assets\\images\\sprites\\stats_bar.png")
         self.level_surface = pygame.Surface((self.stats_bar_img.get_width(), self.stats_bar_img.get_height()-14), pygame.SRCALPHA)
         self.level_surface.fill((255,0,0,128))
-        self.level_text = self.text.draw_text(self.level_surface, f"Level : {self.game.current_level}", 17, (self.level_surface.get_height()/4), 12, "stats_font", (0,255,0))
-        self.timer_text = self.text.draw_text(self.level_surface, "Timer :", 17+ self.level_surface.get_width()/3, (self.level_surface.get_height()/4), 12, "stats_font", (0,255,0)) 
-        self.score_text = self.text.draw_text(self.level_surface, "Score :", 17+ 2*self.level_surface.get_width()/3, (self.level_surface.get_height()/4), 12, "stats_font", (0,255,0))
+        self.level_text = self.text.draw_text(self.level_surface, f"Wave : {self.current_wave}", 17, (self.level_surface.get_height()/4), 12, "stats_font", (0,255,0))
+        self.timer_text = self.text.draw_text(self.level_surface, f"Timer : {self.timer}", 17+ self.level_surface.get_width()/3, (self.level_surface.get_height()/4), 12, "stats_font", (0,255,0)) 
+        self.score_text = self.text.draw_text(self.level_surface, f"Level : {self.game.current_level}", 17+ 2*self.level_surface.get_width()/3, (self.level_surface.get_height()/4), 12, "stats_font", (0,255,0))
 
         self.hp_bar_img = pygame.image.load("assets\\images\\sprites\\Health_Bar_Table.png")
         self.hp_bar_img = pygame.transform.scale(self.hp_bar_img, (158, 27))
@@ -25,6 +27,12 @@ class Hud:
         self.clock_icon_img = pygame.transform.scale(self.clock_icon_img, (25, 26))
         self.clock_icon_rect = self.clock_icon_img.get_rect(center=(self.special_dot_surface.get_width()/2, self.special_dot_surface.get_height()/2))
     
+    def set_current_level(self, wave):
+        self.current_wave = wave
+    
+    def set_current_timer(self, timer):
+        self.timer = timer
+
     def draw_hud(self):
         self.game.screen.blit(self.stats_bar_img, ((self.game.screen.get_width()-self.stats_bar_img.get_width())/2, 0))
         self.game.screen.blit(self.level_surface, ((self.game.screen.get_width()-self.stats_bar_img.get_width())/2, 7))
@@ -34,10 +42,11 @@ class Hud:
         self.game.screen.blit(self.special_dot_img, ((self.game.screen.get_width()-self.special_dot_img.get_width())/2, self.game.screen.get_height()-self.special_dot_img.get_height()))
         self.game.screen.blit(self.special_dot_surface, ((self.game.screen.get_width()-self.special_dot_img.get_width())/2, self.game.screen.get_height()-self.special_dot_img.get_height()))
         self.special_dot_surface.blit(self.clock_icon_img, self.clock_icon_rect)
-    
+
     def update(self):
-        self.level_text = self.text.draw_text(self.level_surface, f"Level : {self.game.current_level}", 17, (self.level_surface.get_height()/4), 12, "stats_font", (0,255,0))
-        self.timer_text = self.text.draw_text(self.level_surface, "Timer :", 17+ self.level_surface.get_width()/3, (self.level_surface.get_height()/4), 12, "stats_font", (0,255,0)) 
-        self.score_text = self.text.draw_text(self.level_surface, "Score :", 17+ 2*self.level_surface.get_width()/3, (self.level_surface.get_height()/4), 12, "stats_font", (0,255,0))
+        self.level_surface.fill((255,0,0,128))
+        self.level_text = self.text.draw_text(self.level_surface, f"Wave : {self.current_wave}", 17, (self.level_surface.get_height()/4), 12, "stats_font", (0,255,0))
+        self.timer_text = self.text.draw_text(self.level_surface, f"Timer : {self.timer}", 17+ self.level_surface.get_width()/3, (self.level_surface.get_height()/4), 12, "stats_font", (0,255,0)) 
+        self.score_text = self.text.draw_text(self.level_surface, f"Level : {self.game.current_level}", 17+ 2*self.level_surface.get_width()/3, (self.level_surface.get_height()/4), 12, "stats_font", (0,255,0))
         self.hp_bar_surface.fill((255,0,0,128))
         self.draw_hud()
