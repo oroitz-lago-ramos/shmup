3b712914d8756d294c288306bba7efd46ae5c7a4
thanh-son-lemelle <thanh-son.lemelle@laplateforme.io>
2024-04-15 12:54:05 +0200

    <refator>:<update Enemy_display.py to load different images based on the phase and update the display accordingly

--- view/Enemy_display.py
+++ view/Enemy_display.py
@@ -6,12 +6,28 @@ class Enemy_display:
         self.width = 40
         self.height = 40
         self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
+        self.load_image()
+
+    def load_image(self):
+        self.image_phase_1 = pygame.image.load("assets\\images\\sprites\\csp_altergeist.png")
+        self.image_phase_1 = pygame.transform.scale(self.image_phase_1, (self.width, self.height))
+
+        self.image_phase_2 = pygame.image.load("assets\\images\\sprites\\sprite_tank-removebg-preview.png")
+        self.image_phase_2 = pygame.transform.scale(self.image_phase_2, (self.width/2, self.height/2))
         
-    def update(self,screen, x, y, width, height, color=(0, 0, 255)):
-        self.rect = pygame.Rect(x, y, width, height)
-        self.rect.center = (x, y)
-        self.draw(screen, color)
-    
+    def update(self,screen, x, y, width, height, second_phase):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        
+        if second_phase == False:
+            self.rect = self.image_phase_1.get_rect(center=(self.x, self.y))
+            screen.blit(self.image_phase_1, self.rect)
+        else:
+
+            screen.blit(self.image_phase_2, (self.x, self.y))
+
     def draw(self, screen, color=(0, 0, 255)):
         pygame.draw.rect(screen, color, self.rect)
         
