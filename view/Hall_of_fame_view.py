232a5b5a7d1662e32e05c6aee7d3c1342d93b9a8
oroitz-lago-ramos <oroitz.lago-ramos@laplateforme.io>
2024-04-15 09:47:16 +0200

    <feat>[Hall_of_fame.py]:<tested surface drawing>

--- view/Hall_of_fame_view.py
+++ view/Hall_of_fame_view.py
@@ -9,6 +9,8 @@ class Hall_of_fame_view:
         self.list = []
         self.last_score = None
         
+        self.hall_of_fame_surface = pygame.Surface((self.game.screen.get_width(), self.game.screen.get_height()))
+        
         self.background = pygame.image.load("assets/images/background/hall_of_fame.png")
         self.background = pygame.transform.scale(self.background, (self.game.screen.get_width(), self.game.screen.get_height()))
         
@@ -31,27 +33,27 @@ class Hall_of_fame_view:
     
         
     def draw(self):
-        self.game.screen.blit(self.background, (0,0))
-        self.game.screen.blit(self.frame, (self.game.screen.get_width() //2 - self.frame.get_width() // 2, 0))
-        self.game.screen.blit(self.little_frame, (self.game.screen.get_width() //2 - self.little_frame.get_width() // 2, self.game.screen.get_height() // 2 - self.little_frame.get_height() // 3 - 41))
-        
+        self.hall_of_fame_surface.blit(self.background, (0,0))
+        self.hall_of_fame_surface.blit(self.frame, (self.hall_of_fame_surface.get_width() //2 - self.frame.get_width() // 2, 0))
+        self.hall_of_fame_surface.blit(self.little_frame, (self.hall_of_fame_surface.get_width() //2 - self.little_frame.get_width() // 2, self.hall_of_fame_surface.get_height() // 2 - self.little_frame.get_height() // 3 - 41))
         for i, score in enumerate(self.list):
-            self.game.screen.blit(self.rectangle, (self.game.screen.get_width() //2 - self.rectangle.get_width() // 2, 220 + i* 125))
-            self.text.draw_text(self.game.screen, f"{score['user_name']} - {int(score['score'])}", self.game.screen.get_width()//2 - 105, 250 + i* 125, 140, "score_font", (255,255,255))
+            self.hall_of_fame_surface.blit(self.rectangle, (self.hall_of_fame_surface.get_width() //2 - self.rectangle.get_width() // 2, 220 + i* 125))
+            self.text.draw_text(self.hall_of_fame_surface, f"{score['user_name']} - {int(score['score'])}", self.hall_of_fame_surface.get_width()//2 - 105, 250 + i* 125, 140, "score_font", (255,255,255))
             if i == 0:
-                self.game.screen.blit(self.gold_star, (self.game.screen.get_width() //2 - self.gold_star.get_width() - 115, 232 + i* 125))
+                self.hall_of_fame_surface.blit(self.gold_star, (self.hall_of_fame_surface.get_width() //2 - self.gold_star.get_width() - 115, 232 + i* 125))
             elif i == 1:
-                self.game.screen.blit(self.silver_star, (self.game.screen.get_width() //2 - self.silver_star.get_width() - 115, 235 + i* 125))
+                self.hall_of_fame_surface.blit(self.silver_star, (self.hall_of_fame_surface.get_width() //2 - self.silver_star.get_width() - 115, 235 + i* 125))
             elif i == 2:
-                self.game.screen.blit(self.empty_star, (self.game.screen.get_width() //2 - self.empty_star.get_width() - 120, 232 + i* 130))
+                self.hall_of_fame_surface.blit(self.empty_star, (self.hall_of_fame_surface.get_width() //2 - self.empty_star.get_width() - 120, 232 + i* 130))
                 
         if self.last_score:
-            self.text.draw_text(self.game.screen, f"LAST SCORE : {self.last_score['user_name']} - {int(self.last_score['score'])}", self.game.screen.get_width()//2 - 430, 60, 250, "last_score_font", (255,255,255))
+            self.text.draw_text(self.hall_of_fame_surface, f"LAST SCORE : {self.last_score['user_name']} - {int(self.last_score['score'])}", self.hall_of_fame_surface.get_width()//2 - 430, 60, 250, "last_score_font", (255,255,255))
             
-        self.game.screen.blit(self.x_button, (self.game.screen.get_width() //2 + self.x_button.get_width() * 4 - 30, self.game.screen.get_height() - self.x_button.get_height() - 60))
+        self.hall_of_fame_surface.blit(self.x_button, (self.hall_of_fame_surface.get_width() //2 + self.x_button.get_width() * 4 - 30, self.hall_of_fame_surface.get_height() - self.x_button.get_height() - 60))
         if self.game.score != None:
-            self.game.screen.blit(self.replay_button, (self.game.screen.get_width() //2 + self.replay_button.get_width() * 5 + 10, self.game.screen.get_height() - self.replay_button.get_height() - 60))    
-      
+            self.hall_of_fame_surface.blit(self.replay_button, (self.hall_of_fame_surface.get_width() //2 + self.replay_button.get_width() * 5 + 10, self.hall_of_fame_surface.get_height() - self.replay_button.get_height() - 60))    
+        
+        self.game.screen.blit(self.hall_of_fame_surface, (0, 0))
     
     def update(self, list, last_score):
         self.list = list
